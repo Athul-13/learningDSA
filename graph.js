058e4a923dbe4c7b4356a6ad8a3641956acefa97
Athul-13 <kkunnikrishnan13@gmail.com>
2026-02-16 10:39:58 +0530

    shortest path

--- graph.js
+++ graph.js
@@ -58,6 +58,71 @@ class Graph {
         }
         return false
     }
+
+    hasCycle(){
+        let parent = {}
+
+        for(let verted in this.adjList){
+            parent[verted] = verted
+        }
+
+        const find = (node) => {
+            if(parent[node] !== node){
+                parent[node] = find(parent[node])
+            }
+            return parent[node]
+        }
+
+        const union = (v1, v2) => {
+            let p1 = find(v1)
+            let p2 = find(v2)
+
+            if(p1 === p2) return true
+
+            parent[p1] = p2
+
+            return false
+        }
+
+        for(let v in this.adjList){
+            for(let n of this.adjList[v]){
+                if(v<n){
+                    if(union(v, n)) return true
+                }
+            }
+        }
+        return false
+    }
+    
+    shortestPath(v1, v2){
+        let queue = [v1]
+        let visited = new Set([v1])
+        let parent = {}
+
+        while(queue.length){
+            let node = queue.shift()
+
+            if(node === v2) break
+
+            for(let n of this.adjList[node]){
+                if(!visited.has(n)){
+                    visited.add(n)
+                    queue.push(n)
+                    parent[n] = node
+                }
+            }
+        }
+
+        let cur = v2
+        let path = []
+
+        while(cur){
+            path.push(cur)
+            cur = parent[cur]
+        }
+
+        return path
+    }
 }
 
 
@@ -84,4 +149,13 @@ console.log('---------------bfs')
 graph.bfs('A')
 
 //has path between two vertices
+console.log('---------------has path')
 console.log(graph.hasPath('A', 'D'))
+
+//has cycle
+console.log('---------------has cycle')
+console.log(graph.hasCycle())
+
+//shortes path in unweighted graph
+console.log('---------------shortes path')
+console.log(graph.shortestPath('A','D'))
